2875b9684680ffa59015835485cec633466f0ab3
anjakefala <anja.kefala@gmail.com>
2019-03-03 01:42:47 -0500

    vdicom dynamically builds the columns from the python object :)

--- vdicom.py
+++ vdicom.py
@@ -8,21 +8,18 @@ def open_dcm(s):
 class DicomSheet(Sheet):
     rowtype = 'metadata'
 
-    columns = [
-            ColumnAttr('tag'),
-            ColumnAttr('keyword'),
-            ColumnAttr('VR', width=0),
-            ColumnAttr('value')
-            ]
-
-    nKeys = 1
-
     def reload(self):
         self.rows = []
+        self.columns = []
 
         dicom_data = pydicom.dcmread(self.source.resolve())
 
         for metadata in dicom_data.iterall():
             self.addRow(metadata)
 
+        for c in PyobjColumns(self.rows[0]):
+            self.addColumn(c)
 
+        self.setKeys([self.column("tag")])
+        for col_name in 'descripWidth is_raw is_undefined_length maxBytesToDisplay showVR file_tell is_retired VM'.split():
+            self.column(col_name).hide()
